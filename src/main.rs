244204574535a4324deda44766cb4c1aba0b6768
dicapisar <dicapisar@gmail.com>
2022-03-02 21:02:20 -0500

    Obtencion de datos de entrada por medio de los env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    let query = &args[2];
+
+    println!("archivo: {}", filename);
+    println!("buscar: {}", query);
 }
